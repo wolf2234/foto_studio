2e05a4f7978552c52b0a02c51d605c634fc2482a
wolf2234 <viktoral9599@gmail.com>
2024-08-21 16:11:54 +0300

    this commit adds employees block.

--- src/js/jquery/slider.js
+++ src/js/jquery/slider.js
@@ -9,4 +9,14 @@ $(document).ready(function () {
         nextArrow: $("#next"),
         prevArrow: $("#prev"),
     });
+    $(".employees__items").slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        adaptiveHeight: true,
+        Infinity: false,
+        autoplay: false,
+        autoplaySpeed: 2000,
+    });
 });
