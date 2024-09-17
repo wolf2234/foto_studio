7ebd33353d1002659499fad256862ed21f0c34e8
wolf2234 <viktoral9599@gmail.com>
2024-09-17 15:54:37 +0300

    this commit adds paths for img.

--- src/js/app.js
+++ src/js/app.js
@@ -5,9 +5,7 @@ import "./jquery/jquery-3.7.1.min.js";
 import "./jquery/slick.min.js";
 import "./jquery/slider.js";
 import "./base/burger.js";
-import { showBlock } from "./modules/functions.js";
-import { clickPhoto } from "./modules/functions.js";
-import { getPosts } from "./modules/functions.js";
+import { showBlock, clickPhoto, getPosts } from "./modules/functions.js";
 
 showBlock(1);
 clickPhoto();
